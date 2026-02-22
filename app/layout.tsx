649f74722b3aa50e9585b2a1f0976fe5e2b5620b
Samet DURSUN <a.sameddursun@outlook.com>
2026-02-23 01:49:20 +0300

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 
@@ -64,6 +65,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 {children}
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
